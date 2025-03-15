2876c12ac2ddc50e46cec17e423478da137ffc8c
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-15 16:07:06 -0400

    Lazer charge sounds

--- src/game/Player.java
+++ src/game/Player.java
@@ -6,6 +6,7 @@ import java.awt.event.KeyEvent;
 import java.awt.geom.Rectangle2D;
 import java.util.Collection;
 
+import game.sound.Sound;
 import game.weapons.NormalWeapon;
 import game.weapons.SpecialWeapon;
 import game.weapons.Weapon;
@@ -15,7 +16,9 @@ public class Player extends DamageEntity {
     private NormalWeapon normal;
     private SpecialWeapon special;
     private int charge;
-    private final int CHARGE_LIMIT = 5000;
+    private Sound chargeSound;
+    private boolean discharged;
+    private final int CHARGE_LIMIT = 500;
     public Player() {
         health = 20;
         normal = new NormalWeapon(this);
@@ -23,7 +26,10 @@ public class Player extends DamageEntity {
         weapon = normal;
         weapon.setAim(1, 0);
         charge = 0;
+        discharged = false;
+        chargeSound = new Sound("src/game/res/sfx/Charge Ready.wav", 0.7f);
     }
+
     public void draw(Graphics2D g2) {
         g2.setColor(Color.BLACK);
         g2.fillRect(x, y, 10, 10);
@@ -34,8 +40,14 @@ public class Player extends DamageEntity {
         y += dy;
         if (weapon.equals(normal) && charge < CHARGE_LIMIT)
             charge++;
-        if (charge <= 0 && weapon.equals(special))
+        if (charge <= 0 && weapon.equals(special)) {
             weapon = normal;
+            discharged = false;
+        }            
+        if (charge == CHARGE_LIMIT && !discharged) {
+            discharged = true;
+            chargeSound.play();
+        }
     }
 
     public void handleKeyInput(Collection<KeyEvent> events) {
