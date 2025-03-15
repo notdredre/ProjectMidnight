eb2b810593e8c021f90d9c31d2c37f54e048d345
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-15 16:02:22 -0400

    Explosion sound

--- src/game/DamageEntity.java
+++ src/game/DamageEntity.java
@@ -1,7 +1,10 @@
 package game;
 
+import game.sound.Sound;
+
 public abstract class DamageEntity extends GameEntity {
     protected int health;
+    protected Sound explosion;
 
     public DamageEntity() {
         this(0, 0);
@@ -9,6 +12,7 @@ public abstract class DamageEntity extends GameEntity {
 
     public DamageEntity(int x, int y) {
         super(x, y);
+        explosion = new Sound("src/game/res/sfx/Explosion 1.wav", 0.7f);
     }
 
     public void tick() {
@@ -22,5 +26,7 @@ public abstract class DamageEntity extends GameEntity {
     
     public void damage(int damage) {
         health -= damage;
+        if (health <= 0)
+            explosion.play();
     }
 }
