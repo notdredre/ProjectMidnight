c7a30ae47f6543b4e126118f5b4f58d2e2613842
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-15 08:01:53 -0400

    Update boss enemy moves

--- src/game/actions/BossMovesFar.java
+++ src/game/actions/BossMovesFar.java
@@ -2,28 +2,43 @@ package game.actions;
 
 import game.BossEnemy;
 import game.GameEntity;
+import game.GameManager;
+import game.TargetPlayer;
 
-public class BossMovesFar extends MoveSet {
+public class BossMovesFar extends MoveSet implements TargetPlayer {
     private final int NUM_STEPS = 1000;
     private BossEnemy enemy;
+    private int playerX, playerY;
     public BossMovesFar(GameEntity owner) {
         super(owner);
         enemy = (BossEnemy) owner;
+        GameManager gameManager = GameManager.getGameManager();
+        gameManager.addTargetPlayer(this);
     }
+
+    public void updatePlayerLocation(int x, int y) {
+        playerX = x;
+        playerY = y;
+    }
+
     @Override
     public void act(int t) {
         int step = t % NUM_STEPS;
         enemy.basicAttack();
-        if (step == 999 || step == 600)
+        if (step == 600)
             enemy.stay();
         if (step > 800) {
             enemy.moveLeft();
         }
         if (step < 600) {
-            enemy.move(0, enemy.getPlayerY() - enemy.getY());
+            enemy.move(0, playerY - enemy.getY());
         }
-        if (step == 999) 
+        if (step == 999) {
+            enemy.stay();
+        }
+        if (Math.abs(playerX - enemy.getX()) < 100 && Math.abs(playerY - enemy.getY()) < 100) {
             enemy.changeMoveSet(new BossMovesClose(owner));
+        } 
     }
     
 }
