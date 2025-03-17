89b5205e4e88980ce7d534cfab262f565ede81a0
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-17 17:28:00 -0400

    Health displayed on overlay

--- src/game/Overlay.java
+++ src/game/Overlay.java
@@ -5,15 +5,18 @@ import java.awt.Graphics2D;
 
 public class Overlay implements Drawable {
     private GameManager gameManager;
-    private String scoreText;
+    private String scoreText, healthText;
 
     public Overlay() {
         gameManager = GameManager.getGameManager();
     }
 
     public void draw(Graphics2D g2) {
+        healthText = "Health: " + gameManager.getHealth();
         scoreText = "Score: " + gameManager.getScore();
+        
         g2.setColor(Color.WHITE);
+        g2.drawString(healthText, 25,375);
         g2.drawString(scoreText, 25, 400);
     }
 }
